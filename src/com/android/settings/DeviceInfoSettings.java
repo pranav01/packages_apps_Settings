f0efe4aea68a759d286c9bf965887682d3237733
Akhil Narang <akhilnarang.1999@gmail.com>
2015-06-04 21:22:43 +0530

    Oops
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -271,7 +271,6 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
             }
         } else if (preference.getKey().equals(KEY_DEVICE_FEEDBACK)) {
             sendFeedback();
-                }
             }
         }
         return super.onPreferenceTreeClick(preferenceScreen, preference);
