ffacd8bf59255a3f38e7ca64b3d51d2e535d05fd
Alex Cruz <mazdarider23@gmail.com>
2015-07-07 18:39:22 +0530

    Selinux Switch (1/1)
    By @Mazda--
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- src/com/android/settings/radium/RadiumIsotopes.java
+++ src/com/android/settings/radium/RadiumIsotopes.java
@@ -25,6 +25,19 @@ import android.preference.PreferenceScreen;
 import android.text.TextUtils;
 import android.util.Log;
 
+import java.io.InputStreamReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.BufferedReader;
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.ContentResolver;
+import android.preference.SwitchPreference;
+import com.android.settings.util.AbstractAsyncSuCMDProcessor;
+import com.android.settings.util.CMDProcessor;
+import com.android.settings.util.Helpers;
+
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
@@ -34,15 +47,48 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class RadiumIsotopes extends SettingsPreferenceFragment {
+public class RadiumIsotopes extends SettingsPreferenceFragment implements
+	Preference.OnPreferenceChangeListener{
 
 private static final String KEY_UPDATE_SETTINGS = "update_settings";
 private static final String KEY_UPDATE_SETTINGS_PACKAGE_NAME = "com.radium.ota";
+private static final String SELINUX = "selinux";
+private SwitchPreference mSelinux;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         addPreferencesFromResource(R.xml.radium_isotopes);
+        PreferenceScreen prefSet = getPreferenceScreen();
+        ContentResolver resolver = getContentResolver();
+	mContext = getActivity();
+
+        //SELinux
+        mSelinux = (SwitchPreference) findPreference(SELINUX);
+        mSelinux.setOnPreferenceChangeListener(this);
+
+        if (CMDProcessor.runSuCommand("getenforce").getStdout().contains("Enforcing")) {
+            mSelinux.setChecked(true);
+            mSelinux.setSummary(R.string.selinux_enforcing_title);
+        } else {
+            mSelinux.setChecked(false);
+            mSelinux.setSummary(R.string.selinux_permissive_title);
+        }
     }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+         if (preference == mSelinux) {
+            if (newValue.toString().equals("true")) {
+                CMDProcessor.runSuCommand("setenforce 1");
+                mSelinux.setSummary(R.string.selinux_enforcing_title);
+            } else if (newValue.toString().equals("false")) {
+                CMDProcessor.runSuCommand("setenforce 0");
+                mSelinux.setSummary(R.string.selinux_permissive_title);
+            }
+            return true;
+          }
+         return false;
+      }
 }
